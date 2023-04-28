028d58d428520b31af97b2fd078562ab8a02465b
Reza Nonva <reza.miraki10@gmail.com>
2023-04-28 22:27:21 +0430

    add FindMaximumSubArray.py

---
+++ Divide & Conquer/FindMaximumSubArray.py
@@ -0,0 +1,40 @@
+
+def maxCrossingSum(arr, low, mid, high):
+	
+    sum = 0
+    left_sum = float('-inf')
+
+    for i in range(mid, low-1, -1):
+        sum = sum + arr[i]
+	
+        if (sum > left_sum):
+            left_sum = sum
+
+    sum = 0
+    right_sum = float('-inf')
+    for i in range(mid, high + 1):
+        sum = sum + arr[i]
+	
+        if (sum > right_sum):
+            right_sum = sum
+            
+    return max(left_sum + right_sum - arr[mid], left_sum, right_sum)
+
+def maxSubArraySum(arr, low, high):
+    if (low > high):
+        return -10000
+    if (low == high):
+        return arr[low]
+    
+    mid = (low + high) // 2
+    return max(maxSubArraySum(arr, low, mid-1),
+			maxSubArraySum(arr, mid+1, high),
+			maxCrossingSum(arr, low, mid, high))
+
+
+arr = [-2, 1, -3, 4, -1, 2, 1, 5, 4]
+n = len(arr)
+
+
+print(maxSubArraySum(arr, 0, n-1))
+
