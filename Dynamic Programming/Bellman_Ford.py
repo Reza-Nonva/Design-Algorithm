1f8ffeca443f9d1e111e96731beb302a1dd1f4f9
Reza Nonva <reza.miraki10@gmail.com>
2023-06-08 20:58:36 +0330

    Create bellman_ford

---
+++ Dynamic Programming/Bellman_Ford.py
@@ -0,0 +1,29 @@
+def bellman_ford(G, source):
+    n = len(G)
+    distance = [float("inf") for _ in range(n)]
+    distance[source] = 0
+    
+    for i in range(n-1):
+        for u in range (n):
+            for v in range(n):
+                if distance[u] != float("inf"):
+                    if (distance[u] + G[u][v] < distance[v]):
+                        distance[v] = distance[u] + G[u][v]
+
+    for u in range (n):
+        for v in range(n):
+            if(distance[u] != float("inf")):
+                if(distance[u] + G[u][v] < distance[v]):
+                    print("graph with negative cycle")
+    print(distance)
+
+x = int(input())
+G = [[0 for _ in range(x)] for _ in range(x)]
+for i in range(x):
+    y = input().split()
+    for j in range (x):
+        if y[j] == "-":
+            G[i][j] = float("inf")
+        else:
+            G[i][j] = int(y[j])
+bellman_ford(G, 0)
