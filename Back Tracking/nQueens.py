7ec23b78095d328bbcdab20c67106fd555f31340
Reza Nonva <reza.miraki10@gmail.com>
2023-06-10 20:23:02 +0330

    Update nQueens.py

--- Back Tracking/nQueens.py
+++ Back Tracking/nQueens.py
@@ -3,28 +3,23 @@ start = time.time()
 
 def nQueens(n):
     answer = [0 for i in range (n+1)]
-    exist = [i for i in range(1, n +1)]
     k = 1
-    bt_nQueens(answer, exist, n, k)
+    bt_nQueens(answer, n, k)
 
 
-def bt_nQueens(answer, exist, n, k):
-    exist_2 = exist.copy()
-
+def bt_nQueens(answer, n, k):
     if k == n + 1:
         print (answer)
         return
     #print(exist)
-    for i in exist:
+    for i in range(1, n+1):
         if(bQueens(answer, k, i)):
             answer[k] = i
-            exist.remove(i)
-            exist_2.remove(i)
-            bt_nQueens(answer, exist_2, n, k+1)
-            exist_2.append(i)
+            bt_nQueens(answer, n, k+1)
+            
 def bQueens(answer, k, i):
+    
     for j in range(1, k):
-        
         if(answer[j] == i or k - i == j - answer[j] or
            k + i == j + answer[j] ):
             return False
