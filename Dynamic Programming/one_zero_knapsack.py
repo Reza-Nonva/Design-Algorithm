47926592e83a898560d5bb05423bf98bfd6087cf
Reza Nonva <reza.miraki10@gmail.com>
2023-06-20 16:25:15 +0330

    create one_zero_knapsack

---
+++ Dynamic Programming/one_zero_knapsack.py
@@ -0,0 +1,28 @@
+def zero_one_knapsack(values, weights, capacity):
+    n = len(values)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            if weights[i - 1] > w:
+                dp[i][w] = dp[i - 1][w]
+            else:
+                dp[i][w] = max(dp[i - 1][w], values[i - 1] + dp[i - 1][w - weights[i - 1]])
+
+    selected_items = []
+    w = capacity
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            selected_items.append(i - 1)
+            w -= weights[i - 1]
+
+    return dp[n][capacity], selected_items[::-1]
+
+
+values = [60, 100, 120]
+weights = [10, 20, 30]
+capacity = 50
+
+max_value, selected_items = zero_one_knapsack(values, weights, capacity)
+print("Maximum value:", max_value)
+print("Selected items:", selected_items)
